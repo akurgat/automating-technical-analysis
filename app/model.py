306d70ac6373154dc36f56d38c7822ad91c984ba
Andrew Kurgat <47039863+akurgat@users.noreply.github.com>
2021-05-18 01:25:18 +0300

    Version 3.0

--- app/model.py
+++ app/model.py
@@ -51,7 +51,7 @@ class Prediction(Preprocessing):
         self.df_visulization['Action_Predictions'] = self.model_prediction_action
         self.df_visulization = self.df_visulization[['Adj Close', 'Volume', 'Distinct_Action', 
                                                      'Action_Predictions', 'Future_Adj_Close']]
-        self.df_visulization = self.df_visulization.iloc[-366:]
+        #self.df_visulization = self.df_visulization.iloc[-366:]
         self.df_visulization['Price_Buy'] = self.df_visulization[self.df_visulization[indicators[self.indication]] == 'Buy']['Adj Close']
         self.df_visulization['Price_Sell'] = self.df_visulization[self.df_visulization[indicators[self.indication]] == 'Sell']['Adj Close']
 
